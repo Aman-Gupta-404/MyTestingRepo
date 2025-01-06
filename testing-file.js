078f5a067fee70d0386c5bf94dca1cd5925329a9
Aman Gupta <63041409+Aman-Gupta-404@users.noreply.github.com>
2025-01-06 17:34:39 +0530

    Added a file for testing

---
+++ testing-file.js
@@ -0,0 +1,9 @@
+console.log("This is a testing file for my assignment");
+
+const arr = [1, 2, 3, 4];
+
+arr.map(item => {
+    console.log("logging it - ", item)
+})
+
+console.log("This is the end of the code!")
